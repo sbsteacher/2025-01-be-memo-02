30b4e5cf036335a50e3fc262f5f3395d8951bea3
sbsteacher <id@gmail.com>
2025-06-27 14:21:24 +0900

    완료2

--- src/main/java/com/green/memoserver/model/MemoGetOneRes.java
+++ src/main/java/com/green/memoserver/model/MemoGetOneRes.java
@@ -1,11 +1,15 @@
 package com.green.memoserver.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
 public class MemoGetOneRes {
     private int id;
     private String title;
+    @JsonProperty("ctnts")
     private String content;
+    @JsonProperty("created_at")
     private String createdAt;
 }
